8e3eec2f7d3e75d8ae576cd2fb416ab37d2f7eb5
Stevan Little <stevan.little@gmail.com>
2026-02-28 12:14:16 +0100

    syntax files are annoying, but this is some progress

--- editors/install-syntax-file.sh
+++ editors/install-syntax-file.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/MXCL/MXCL.sublime-syntax
 cp ./editors/sublime/MXCL.sublime-syntax ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/MXCL/MXCL.sublime-syntax
